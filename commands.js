1a7e5e814a1ba4fc0579adeb2c10b8582ac31ca3
Saibou <saiboub80@gmail.com>
2023-04-02 11:01:58 -0400

    update commands.js

--- commands.js
+++ commands.js
@@ -1,37 +1,62 @@
 // JASKIS
 // paste the MongoDB commands you use underneath each prompt
-
+db
 // GETTING STARTED
 // 1. Create a database called jaskis
-
+use Jaskis
+'switched to db Jaskis'
+db
+Jaskis
 
 // 2. Create a collection called bounties
+db.createCollection('bounties'),
+show collections,
+
 
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
+db.bounties.insertOne()
 
 
 // 2. Query for all bounties in the bounties collection
-
+db.bounties.insertMany()
 
 // 3. Insert many bounties at once using the given objects
-
+db.bounties.insertMany()
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
+db.bounties.find({location:"Grasslands"})
 
 // 2. Query for all bounties with a reward worth 10000 or more
+ned
+db.bounties.find({reward:{$gt:10000}})
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
+db.bounties.deleteMany({client:"Red wolf"})
 
 // 4. Query for a Groundhog in the Woodlands
+db.bounties.findOne({
+    $and: [
+      { species: 'Groundhog' },
+      { location: 'Woodlands' },
+    ],
+  })
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+db.bounties.updateOne({ name: 'Polarwind' },
+      { $set: { reward: 10000 } })
+
 
 // 2. Remove Lokinkajou
+db.bounties.deleteOne({name:'Lokinkajou'})
 
 // 3. Delete all bounties sent by Songbird
+db.bounties.deleteMany({client:'Songbird'})
 
 // 4. Update all captured statuses to true
+db.bounties.updateMany({},
+    { $set: { captured: true } }
+  )
